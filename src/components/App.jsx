666af9e227895edeb42a0d080556119f54de4224
KrystynaShevchenko <119902380+KrystynaShevchenko@users.noreply.github.com>
2024-05-01 19:23:00 +0300

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,26 @@
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import ContactForm from "./ContactForm";
+import SearchBox from "./SearchBox";
+import ContactList from "./ContactList";
+import { fetchContacts } from "../redux/contactsOps";
+
+import "./App.css";
+
+const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
+    </div>
+  );
+};
+export default App;
